a9f1f574c493d9c7c19509207cf4d3ed89dc906f
sirajuddinrbg11-bit <sirajuddinrbg11@gmail.com>
2026-06-04 23:14:11 -0700

    Update pharma.py

--- pharma.py
+++ pharma.py
@@ -11,14 +11,14 @@ def predict_drug_response(features):
   return prediction
 
 
-st.title("Drug Response Prediciton")
+st.title("Drug Response Prediction")
 
 # get user Input
-DD=st.number_input("Drug Dosage in mg",min_value=0.0)
-SBP=st.number_input("Systolic Blood Pressure",min_value=0.0)
-HR=st.number_input("Heart Rate (BPM)",min_value=0.0,max_value=5)
-LTI=st.number_input("Liver Toxicity Index (U/L)",min_value=0.0)
-BGL=st.number_input("Blood Glucose Level",min_value=0.0)
+DD=st.number_input("Drug Dosage in mg",value=0.0)
+SBP=st.number_input("Systolic Blood Pressure",value=0.0)
+HR=st.number_input("Heart Rate (BPM)",min_value=0.0,value=5)
+LTI=st.number_input("Liver Toxicity Index (U/L)",value=0.0)
+BGL=st.number_input("Blood Glucose Level",value=0.0)
 
 
 
